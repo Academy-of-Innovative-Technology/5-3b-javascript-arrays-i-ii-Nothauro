e9d3f0009e86eefca09b898bce750a86b66e34aa
Thad <Thadphillp35@gmail.com>
2026-05-18 12:39:05 -0400

    Almost done with part 1

---
+++ script.js
@@ -0,0 +1,10 @@
+const fruits = ["apple", "banana", "cherry", "date"];
+const removedElement = fruits.shift();
+const removedFruit = fruits.pop(); 
+
+console.log(fruits);
+console.log(removedElement);
+ 
+fruits.unshift("Apricot");
+
+fruits.pop
